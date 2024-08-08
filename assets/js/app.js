6e7bbc2fe339f5681f369f47fb2873da95b3825e
tusharswami534 <tusharswami17@gmail.com>
2024-08-08 21:30:56 +0530

    nanv bar complete

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navmenu = () => {
+    document.getElementById("rotate").classList.toggle("rotate")
+    document.getElementById("rotate-1").classList.toggle("rotate-1")
+    document.getElementById("line-2").classList.toggle("bg-transparent")
+    document.getElementById("show-nav").classList.toggle("end-0")
+
+}
